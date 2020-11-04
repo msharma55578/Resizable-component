bbff6eb46c6bab1374b72c632faab211d4f190a5
msharma55578 <m.sharma55578@gmail.com>
2020-11-04 12:27:33 +0530

    final commit 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { Resizable } from "re-resizable";
 
+const style = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  border: "solid 5px #ddd",
+  background: "#eaa2d2"
+};
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div className="displayclass">
+      <Resizable
+        style={style}
+        defaultSize={{
+          width: 700,
+          height: 400
+        }}
+      >
+        Component 1
+      </Resizable>
+      <Resizable
+        style={style}
+        defaultSize={{
+          width: 700,
+          height: 400,
+        }}
+      >
+        Component 2
+      </Resizable>
     </div>
+    <Resizable
+      style={style}
+      defaultSize={{
+        width: 1400,
+        height: 250
+      }}
+    >
+      Component 3
+    </Resizable>
+  </div>
   );
 }
 
